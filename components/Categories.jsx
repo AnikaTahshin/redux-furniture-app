4531b5e11830a85a0c77a5fca0a91a1ece364e5c
AnikaTahshin <anikatahsinmeem@gmail.com>
2024-11-28 20:44:59 +0600

    categories section added

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import category from '../public/img/Category/categories.json'
+import Image from 'next/image'
 
 const Categories = () => {
+
+    console.log("see cater",category)
   return (
-    <div>Categories</div>
+    <div className='w-full grid grid-cols-8 gap-3 py-5 px-2'>
+
+        {
+            category.map((element,index) => (
+                <div key={index} className="flex flex-col justify-center items-center h-32 w-32 p-3 rounded-full bg-lime-300 hover:bg-lime-500 transition-all duration-500 ease-linear cursor-pointer">
+                   <Image src={element.image} alt={element.name} height={20} width={30} />
+                <h1 className='text-sm py-2 text-nowrap text-center font-bold'>{element.name}</h1>
+                </div>
+            ))
+        }
+    </div>
   )
 }
 
--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,12 +1,20 @@
 "use client";
 import React from "react";
 import HeroSlider from "./HeroSlider";
-
+import Categories from "./Categories";
 const HomePage = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex flex-col justify-center items-center">
       <div className="w-full">
         <HeroSlider />
+      </div>
+      <div className="w-11/12 py-5">
+      <div className="title_container py-2">
+        <h1 className="title font-bold text-3xl">
+          Find Your Style:Furniture Categories
+        </h1>
+      </div>
+        <Categories />
       </div>
     </div>
   );
